1b4e4248bc59d93db5cdc4578af3643acf793095
7minutech <elijohnson1107@gmail.com>
2024-09-08 11:50:02 -0500

    refactor: now huffman will contain word
    Instead of passing word to the method for mapping
    the huffman object will hold it

--- spec/huffman_spec.rb
+++ spec/huffman_spec.rb
@@ -3,8 +3,8 @@ require "./lib/huffman"
 describe Huffman do
   describe "map_frequency" do
     it "returns string given to list with hash with key value pairs" do
-      my_huff = Huffman.new
-      my_huff.map_frequency("AABCBAD").to eql([{ A: 3 }, { B: 2 }, { C: 1 }, { D: 1 }])
+      my_huff = Huffman.new("AABCBAD")
+      my_huff.map_frequency.to eql([{ A: 3 }, { B: 2 }, { C: 1 }, { D: 1 }])
     end
   end
 end
