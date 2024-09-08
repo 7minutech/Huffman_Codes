6bb0e26e8196e93ed8ddebe3a9fe5a10fe860c7f
7minutech <elijohnson1107@gmail.com>
2024-09-08 12:13:06 -0500

    fix: Added expect

--- spec/huffman_spec.rb
+++ spec/huffman_spec.rb
@@ -4,7 +4,7 @@ describe Huffman do
   describe "map_frequency" do
     it "returns string given to list with hash with key value pairs" do
       my_huff = Huffman.new("AABCBAD")
-      my_huff.map_frequency.to eql([{ A: 3 }, { B: 2 }, { C: 1 }, { D: 1 }])
+      expect(my_huff.map_frequency).to eql({ A: 3, B: 2, C: 1, D: 1 })
     end
   end
 end
