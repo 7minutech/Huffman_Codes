7d12049039f9e038e6de248a2e5536bb99c4ad49
7minutech <elijohnson1107@gmail.com>
2024-09-15 19:16:53 -0500

    feat: added additional test

--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -7,4 +7,10 @@ describe "creat encoding" do
     my_huff.tree.pretty_print
     expect(my_huff.tree.create_encoding("A")).to eql([1, 0])
   end
+  it "works with differnt words" do
+    my_huff = Huffman.new("AAABBCD")
+    my_huff.create_tree
+    my_huff.tree.pretty_print
+    expect(my_huff.tree.create_encoding("A")).to eql([0])
+  end
 end
